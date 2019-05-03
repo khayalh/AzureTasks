136161a0389d66470be353b5d45b1f81cf6c36e5
khayalh <xayal444@gmail.com>
2019-05-03 10:29:07 +0400

    changing DSC resource

--- Task6/Task6/DSC/DSC.ps1
+++ Task6/Task6/DSC/DSC.ps1
@@ -9,10 +9,10 @@ Configuration Main
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     # Import-DscResource -ModuleName xPSDesiredStateConfiguration
     Find-Module xPSDesiredStateConfiguration | Install-Module
+    Find-Module xWebAdministration | Install-Module -Force
     Import-DscResource -ModuleName xWebAdministration
-    # Find-Module xWebAdministration | Install-Module
+    Find-Module xCertificate | Install-Module -Force
     Import-DscResource -ModuleName xCertificate
-    # Find-Module xCertificate | Install-Module
 
     File ArtifactsFolder {
         Type            = "Directory"
