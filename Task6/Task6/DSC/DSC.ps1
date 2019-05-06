1350e09ce4e947e22b4952139337785f87604d0a
khayalh <xayal444@gmail.com>
2019-05-06 18:27:44 +0400

    changing Task6 config

--- Task6/Task6/DSC/DSC.ps1
+++ Task6/Task6/DSC/DSC.ps1
@@ -1,6 +1,3 @@
-$password = "certpass" | ConvertTo-SecureString -asPlainText -Force
-$username = "user"
-[PSCredential] $certcredential = New-Object System.Management.Automation.PSCredential($username,$password)
 Configuration Main
 {
 
@@ -14,77 +11,84 @@ Configuration Main
     Import-DscResource -ModuleName xWebAdministration
     Import-DscResource -ModuleName xCertificate
 
-    File ArtifactsFolder {
-        Type            = "Directory"
-        DestinationPath = "C:\Cert"
-        Ensure          = "Present"
-    }   
-    xRemoteFile DownloadPackage {            	
-        DestinationPath = "C:\Cert\selfsignedcert.pfx"
-        Uri             = $certfilelocation
-        MatchSource     = $true
-        DependsOn       = "[File]ArtifactsFolder" 
-    }
-    WindowsFeature IIS
-    {
-        Ensure          = "Present"
-        Name            = "Web-Server"
-    }
-    WindowsFeature Management {
+    Node $nodeName {
+        
+        File ArtifactsFolder {
+            Type            = "Directory"
+            DestinationPath = "C:\Cert"
+            Ensure          = "Present"
+        }   
+        xRemoteFile DownloadPackage {            	
+            DestinationPath = "C:\Cert\selfsignedcert.pfx"
+            Uri             = $certfilelocation
+            MatchSource     = $true
+            DependsOn       = "[File]ArtifactsFolder" 
+        }
+        WindowsFeature IIS {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+        WindowsFeature Management {
  
-        Name = 'Web-Mgmt-Service'
-        Ensure = 'Present'
-        DependsOn = @('[WindowsFeature]IIS')
-    }
-    Registry RemoteManagement {
-        Key = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'
-        ValueName = 'EnableRemoteManagement'
-        ValueType = 'Dword'
-        ValueData = '1'
-        DependsOn = @('[WindowsFeature]IIS','[WindowsFeature]Management')
-    }
-    Service StartWMSVC {
-        Name = 'WMSVC'
-        StartupType = 'Automatic'
-        State = 'Running'
-        DependsOn = '[Registry]RemoteManagement'
-    }
-    # Stop the default website
-    xWebsite DefaultSite
-    {
-        Ensure          = "Present"
-        Name            = "Default Web Site"
-        State           = "Stopped"
-        PhysicalPath    = "C:\inetpub\wwwroot"
-        DependsOn       = "[WindowsFeature]IIS"
-    }
-    xPfxImport ImportPfxCert
-    {
-    Thumbprint = 'FB7E2DBDA1D2F41A63273C684DFA92D2699AC6EB'
-    Path = 'C:\Cert\selfsignedcert.pfx'
-    Credential = $CertCredential.Password
-    Location = 'LocalMachine'
-    Store = "My"
-    DependsOn = '[WindowsFeature]IIS'
+            Name      = 'Web-Mgmt-Service'
+            Ensure    = 'Present'
+            DependsOn = @('[WindowsFeature]IIS')
+        }
+        Registry RemoteManagement {
+            Key       = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'
+            ValueName = 'EnableRemoteManagement'
+            ValueType = 'Dword'
+            ValueData = '1'
+            DependsOn = @('[WindowsFeature]IIS', '[WindowsFeature]Management')
+        }
+        Service StartWMSVC {
+            Name        = 'WMSVC'
+            StartupType = 'Automatic'
+            State       = 'Running'
+            DependsOn   = '[Registry]RemoteManagement'
+        }
+        # Stop the default website
+        xWebsite DefaultSite {
+            Ensure       = "Present"
+            Name         = "Default Web Site"
+            State        = "Stopped"
+            PhysicalPath = "C:\inetpub\wwwroot"
+            DependsOn    = "[WindowsFeature]IIS"
+        }
+        xPfxImport ImportPfxCert {
+            Thumbprint = 'FB7E2DBDA1D2F41A63273C684DFA92D2699AC6EB'
+            Path       = 'C:\Cert\selfsignedcert.pfx'
+            Credential = $CertCredential
+            Location   = 'LocalMachine'
+            Store      = "WebHosting"
+            DependsOn  = '[WindowsFeature]IIS'
 
-    }
-    # Create the new Website with HTTP
-    xWebsite NewWebsite
-    {
-        Ensure          = "Present"
-        Name            = "iissrv.westeurope.cloudapp.azure.com"
-        State           = "Started"
-        PhysicalPath    = "C:\inetpub\wwwroot"
-        DependsOn       = "[WindowsFeature]IIS",'[xPfxImport]ImportPfxCert'
-        BindingInfo     = @(
-            MSFT_xWebBindingInformation
-            {
-                Protocol              = "https"
-                Port                  = 443
-                HostName              = "iissrv.westeurope.cloudapp.azure.com"
-                CertificateThumbprint = "FB7E2DBDA1D2F41A63273C684DFA92D2699AC6EB"
-                CertificateStoreName  = "My"
-            }
-        )
+        }
+        # Create the new Website with HTTP
+        xWebsite NewWebsite {
+            Ensure       = "Present"
+            Name         = "iissrv.westeurope.cloudapp.azure.com"
+            State        = "Started"
+            PhysicalPath = "C:\inetpub\wwwroot"
+            DependsOn    = "[WindowsFeature]IIS", '[xPfxImport]ImportPfxCert'
+            BindingInfo  = @(
+                MSFT_xWebBindingInformation {
+                    Protocol              = "https"
+                    Port                  = 443
+                    HostName              = "iissrv.westeurope.cloudapp.azure.com"
+                    CertificateThumbprint = "FB7E2DBDA1D2F41A63273C684DFA92D2699AC6EB"
+                    CertificateStoreName  = "WebHosting"
+                }
+            )
+        }
     }
 }
+$cd = @{
+    AllNodes = @(
+        @{
+            NodeName                    = '*';
+            PSDscAllowPlainTextPassword = $true;
+        }
+    )
+}
+Main -ConfigurationData $cd
