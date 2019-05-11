8d19b370d173bdb526c5d87f1752b63bd7dfeb0b
khayalh <xayal444@gmail.com>
2019-05-11 22:54:41 +0400

    add Task8

--- Task8/task8/AzureResourceGroup6/VirtualNetworkPeering.ps1
+++ Task8/task8/AzureResourceGroup6/VirtualNetworkPeering.ps1
@@ -1,4 +1,3 @@
-Login-AzureRmAccount
 $vm1vnet=Get-AzureRmVirtualNetwork -ResourceGroupName task8Rg1 -Name vmnetwork
 $vm2vnet=Get-AzureRmVirtualNetwork -ResourceGroupName task8Rg2 -Name vm2VNET
 Add-AzureRmVirtualNetworkPeering -Name "myVnet1ToMyVnet2" -VirtualNetwork $vm1vnet -RemoteVirtualNetworkId $vm2vnet.Id
